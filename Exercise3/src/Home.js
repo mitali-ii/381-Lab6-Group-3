e55e26d0f57ea0e5a95312264af007fc6038d456
alisonzarazua <98575863+alisonzarazua@users.noreply.github.com>
2025-03-07 12:17:14 -0700

    Add files via upload

---
+++ Exercise3/src/Home.js
@@ -0,0 +1,11 @@
+function Home({title, description}) {
+    return (
+      <div className="App">
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+
